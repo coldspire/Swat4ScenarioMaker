a623eaa6450d99b52d3c6f189c2a70f8d210bf9d
Owen Anderson <5332788+coldspire@users.noreply.github.com>
2019-07-26 22:15:32 -0400

    Add source maps in development

--- webpack.config.js
+++ webpack.config.js
@@ -2,40 +2,45 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = {
-    entry: {
-        app: './app/index.js',
-        generator: './app/scripts/generator.js'
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            inject: false,
-            template: require('html-webpack-template'),
+module.exports = (env, options) => {
+    const isProduction = (options && options.mode === 'production') || false;
 
-            lang: 'en-US',
-            meta: [
-                { 
-                    name: 'viewport',
-                    content: 'width=device-width, initial-scale=1, shrink-to-fit=no'
+    return {
+        entry: {
+            app: './app/index.js',
+            generator: './app/scripts/generator.js'
+        },
+        devtool: (isProduction ? '' : 'eval-source-map'),
+        plugins: [
+            new CleanWebpackPlugin(),
+            new HtmlWebpackPlugin({
+                inject: false,
+                template: require('html-webpack-template'),
+
+                lang: 'en-US',
+                meta: [
+                    { 
+                        name: 'viewport',
+                        content: 'width=device-width, initial-scale=1, shrink-to-fit=no'
+                    }
+                ],
+                title: 'SWAT 4 Scenario Generator',
+            })
+        ],
+        output: {
+            filename: '[name].bundle.js',
+            path: path.resolve(__dirname, 'dist')
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.css$/,
+                    use: [
+                        'style-loader',
+                        'css-loader'
+                    ]
                 }
-            ],
-            title: 'SWAT 4 Scenario Generator',
-        })
-    ],
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            }
-        ]
-    }
+            ]
+        }
+    };
 };
