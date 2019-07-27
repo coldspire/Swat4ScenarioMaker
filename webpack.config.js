4b939271e3fe50ad7f15f8e976ea467ef4023b8d
Owen Anderson <5332788+coldspire@users.noreply.github.com>
2019-07-26 21:44:57 -0400

    Add webpack bundling, HTML gen, cleaning

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,30 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    entry: './app/index.js',
+    entry: {
+        app: './app/index.js',
+        generator: './app/scripts/generator.js'
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            inject: false,
+            template: require('html-webpack-template'),
+
+            lang: 'en-US',
+            meta: [
+                { 
+                    name: 'viewport',
+                    content: 'width=device-width, initial-scale=1, shrink-to-fit=no'
+                }
+            ],
+            title: 'SWAT 4 Scenario Generator',
+        })
+    ],
     output: {
-        filename: 'index.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
     module: {
