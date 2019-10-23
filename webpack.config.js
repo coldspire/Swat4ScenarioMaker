1f64bcc67b4ea6ab30c229a1062aef811023eb6f
Owen <5332788+coldspire@users.noreply.github.com>
2019-10-22 20:27:40 -0400

    Make UI more SWAT-like (#11)
    * Add font for titles and labels
    
    * Options updates
    
    * Add custom checkboxes
    
    * Change options to be unselectable
    
    * Add background with copyfileloader
    
    * UI polish, or at least changes
    
    * Change location to look like directives
    
    * Fix background and header
    
    * Add mobile layout for options
    
    * Polish body location and move options hover
    
    * Shrink directive headers a bit
    
    * Add header coloring and compression
    
    * Modify header to adjust segments separately

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = (env, options) => {
     const isProduction = (options && options.mode === 'production') || false;
@@ -18,7 +19,10 @@ module.exports = (env, options) => {
             new HtmlWebpackPlugin({
                 inject: false,
                 template: './app/index.html'
-            })
+            }),
+            new CopyWebpackPlugin([
+              { from: 'app/assets/images', to: 'assets/images' }
+            ]),
         ],
         output: {
             filename: '[name].bundle.js',
