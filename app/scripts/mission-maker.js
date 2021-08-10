b46e13d4e8b7a2a0291dab58a85eeb2984e16fac
Owen Anderson <5332788+coldspire@users.noreply.github.com>
2021-08-09 21:01:50 -0400

    Add some fill-ins

--- app/scripts/mission-maker.js
+++ app/scripts/mission-maker.js
@@ -1,41 +1,83 @@
+const niceity =
+  'Okay guys, listen up. OCID (Organized Crime Investigation Division) has spent a lot of time setting this one up, and they’ve asked for our help serving the arrest warrant.';
+
+const locationName = {
+  properName: 'Food Wall',
+  buildingType: 'restaurant',
+  buildingOther: 'residence',
+};
+
+const weapon = {
+  generic: 'gun',
+  specific: 'MAC-10 submachine gun',
+  plural: 'firearms',
+  pluralShort: 'arms',
+  accessory: 'high-volume magazines',
+};
+
+const detectiveName = {
+  first: 'Detective',
+  last: 'Sheary',
+  get full() {
+    return `${this.first} ${this.last}`;
+  },
+};
+
+const criminalNameA = {
+  first: 'Lian',
+  last: 'Niu',
+  get full() {
+    return `${this.first} ${this.last}`;
+  },
+};
+
+const criminalNameB = {
+  first: 'Alex',
+  last: 'Jiminez',
+  nickname: 'The Knife',
+  get full() {
+    return `${this.first} ${this.last}`;
+  },
+  descriptors: ['armed', 'dangerous'],
+};
+
+const crime = {
+  singular: 'homicide',
+  plural: 'homicides',
+};
+
 const missionTemplatePara1 = `
-    Okay guys, listen up. OCID (Organized Crime Investigation Division) has spent a lot of time setting this one up, 
-    and they’ve asked for our help serving the arrest warrant. 
-    For the last two weeks, the Food Wall Restaurant has been under close surveillance. 
-    We believe the owner, one Lian Niu, an experienced gunsmith, deals in the illegal modification of firearms,
-    in this case, fabricating custom high-volume magazines.
-    Niu maintains a residence above the restaurant, and does his business there.
-    He’s been questioned about illegal arms before, but walked.
+    ${niceity}
+    For the last two weeks, the ${locationName.properName} ${locationName.buildingType} has been under close surveillance. 
+    We believe the owner, one ${criminalNameA.full}, an experienced ${weapon.generic}smith, deals in the illegal modification of ${weapon.plural},
+    in this case, fabricating custom ${weapon.accessory}.
+    ${criminalNameA.last} maintains a ${locationName.buildingOther} above the ${locationName.buildingType}, and does his business there.
+    He’s been questioned about illegal ${weapon.pluralShort} before, but walked.
 `;
 
 const missionTemplatePara2 = `
-    Ten minutes ago, Detective Sheary sighted Alex Jimenez entering the restaurant. 
-    Look at your files; Alex Jimenez, also known as “The Knife,” is a hit man with Colombian connections. 
-    He’s a key suspect in multiple homicides over the last year and half. 
-    Jimenez should be considered armed and dangerous.
+    Ten minutes ago, ${detectiveName.full} sighted ${criminalNameB.full} entering the ${locationName.buildingType}. 
+    Look at your files; ${criminalNameB.full}, also known as “${criminalNameB.nickname},” is a hit man with Colombian connections. 
+    He’s a key suspect in multiple ${crime.plural} over the last year and half. 
+    ${criminalNameB.last} should be considered ${criminalNameB.descriptors[0]} and ${criminalNameB.descriptors[1]}.
 `;
 
 const missionTemplatePara3 = `
-    Sheary believes that Jimenez is presently inside the business, 
-    having modifications made to a MAC-10 submachine gun used in a recent homicide. 
-    We want the evidence intact, so we can’t give Jimenez and Niu any warning. 
-    The restaurant’s patrons and employees will still be in the building when you go in. 
+    ${detectiveName.last} believes that ${criminalNameB.last} is presently inside the business, 
+    having modifications made to a ${weapon.specific} used in a recent ${crime.singular}. 
+    We want the evidence intact, so we can’t give ${criminalNameB.last} and ${criminalNameA.last} any warning. 
+    The ${locationName.buildingType}’s patrons and employees will still be in the building when you go in. 
     As always, target ID.
 `;
 
 const missionTemplatePara4 = `
     Your first priority is, of course, to avoid any civilian injuries; 
-    but after that, secure the gun in Jimenez’s possession. 
-    OCID is looking forward to having a long talk with both Jimenez and Niu — 
-    and that gun is just the thing to make them chatty. 
+    but after that, secure the ${weapon.generic} in ${criminalNameB.last}’s possession. 
+    OCID is looking forward to having a long talk with both ${criminalNameB.last} and ${criminalNameA.last} — 
+    and that ${weapon.generic} is just the thing to make them chatty. 
     So take a careful look at the floor plans, plan your entry, and gear up.
 `;
 
 export function getMission() {
-  return [
-    missionTemplatePara1,
-    missionTemplatePara2,
-    missionTemplatePara3,
-    missionTemplatePara4
-  ];
+  return [missionTemplatePara1, missionTemplatePara2, missionTemplatePara3, missionTemplatePara4];
 }
